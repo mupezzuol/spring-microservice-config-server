f315001f04adfb0dac183ce5e79675116d53d79e
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-01-06 20:23:34 -0300

    Add config for searching in fileSystem

--- src/main/java/br/com/microservice/config/SpringMicroserviceConfigServerApplication.java
+++ src/main/java/br/com/microservice/config/SpringMicroserviceConfigServerApplication.java
@@ -2,8 +2,10 @@ package br.com.microservice.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringMicroserviceConfigServerApplication {
 
 	public static void main(String[] args) {
